10575ca6993072e1c717919f5a2090b748e78d5f
GaneshShiv <gnsh.shiv@gmail.com>
2020-07-17 23:21:24 +0530

    my project

--- src/test/java/org/MyProjectJuly17/July172020.java
+++ src/test/java/org/MyProjectJuly17/July172020.java
@@ -3,8 +3,11 @@ package org.MyProjectJuly17;
 public class July172020 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+	System.out.println("git");	
 
-	}
 
+
+	
+}
 }
